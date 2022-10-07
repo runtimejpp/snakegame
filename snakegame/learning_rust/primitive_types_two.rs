84d24b8b312f166614f0d4328fd8bc56fbda9e42
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-10-06 22:08:53 -0400

    Create primitive_types_two.rs

---
+++ snakegame/learning_rust/primitive_types_two.rs
@@ -0,0 +1,17 @@
+    fn main(){
+        let a = 10;
+        let b = a;
+        let c = 15;
+        let d = add(a, b);
+
+        println!("{}", sum);
+
+    }
+
+    fn add(x: u32, y: u32) -> u32 {
+        let sum = x + y;
+      //  return sum;
+        sum;
+    
+
+    }
