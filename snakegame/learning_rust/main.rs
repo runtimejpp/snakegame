c35cebf382f7386d9274175d47428325ec33a684
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-10-06 21:56:31 -0400

    Create main.rs

---
+++ snakegame/learning_rust/main.rs
@@ -0,0 +1,14 @@
+// primitive types
+
+  fn main(){
+      let custom_number = 98_000;
+      let hex_number = 0xfa;
+      let binary_number = 0b0010_1011;
+      let byte_count = b'A';
+      
+      println!("{}", custom_number);
+      println!("{}", hex_number);
+      println!("{}", binary_number);
+      println!("{}", byte_number);
+      
+  }
