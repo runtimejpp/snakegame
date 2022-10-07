9bbfe5a7d90d2a7a26284e8ea5f84013f77cd0ec
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-10-06 21:59:27 -0400

    Update main.rs

--- snakegame/learning_rust/main.rs
+++ snakegame/learning_rust/main.rs
@@ -1,10 +1,10 @@
 // primitive types
 
   fn main(){
-      let custom_number = 98_000;
-      let hex_number = 0xfa;
-      let binary_number = 0b0010_1011;
-      let byte_number = b'A';
+      let custom_number = 98_000;                   // 98,000
+      let hex_number = 0xfa;                        // 250
+      let binary_number = 0b0010_1011;              // 43
+      let byte_number = b'A';                       // 65
       
       println!("{}", custom_number);
       println!("{}", hex_number);
